39d7e834e4467fa71abdd76fde480c1eab418d3b
zmadson3 <zmadson3@gatech.edu>
2025-02-19 09:42:37 -0500

    Initial Commit

---
+++ DailyWorkout/DailyWorkoutApp.swift
@@ -0,0 +1,20 @@
+//
+//  DailyWorkoutApp.swift
+//  DailyWorkout
+//
+//  Created by Zeko Madson on 2/19/25.
+//
+
+import SwiftUI
+
+@main
+struct DailyWorkoutApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
