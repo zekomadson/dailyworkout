848c747a8e0f24f3b310718a19aabd4510351a39
zmadson3 <zmadson3@gatech.edu>
2025-02-22 11:17:51 -0500

    Removal of old code and addition of a few text boxes.

---
+++ DailyWorkout/SignUpView.swift
@@ -0,0 +1,22 @@
+//
+//  SignUpView.swift
+//  DailyWorkout
+//
+//  Created by Zeko Madson on 2/19/25.
+//
+
+import SwiftUI
+import CoreData
+
+struct SignUpView: View {
+
+    var body: some View {
+        VStack {
+            // Header
+            Text("Daily Workout")
+                .font(.largeTitle)
+            
+            Spacer()
+        }
+    }
+}
