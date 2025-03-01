75383ade7dec9383adfde738768830a02891dc76
zmadson3 <zmadson3@gatech.edu>
2025-03-01 12:26:01 -0500

    Addition of more quotes

--- DailyWorkout/WorkoutPage.swift
+++ DailyWorkout/WorkoutPage.swift
@@ -56,128 +56,160 @@ struct WorkoutPage: View {
         , "Consistency over intensity.\nProgress over perfection.\nFundamentals over fads."
         , "Start over as many times as you need to."
         , "People do not decide their futures, they decide their habits and their habits decide their futures"
+        , "Excellence is never an accident. It's always the result of high intention, sincere effort and intelligent execution."
+        , "Go confidently in the direction of your dreams. Live the life you have imagined."
+        , "The calmer you are, the clearer you think."
+        , "You are what you do, not what you say you'll do."
+        , "If you want to make the wrong decision, ask everyone."
+        , "The measure of intelligence is the ability to change."
+        , "May your choices reflect your hopes, not your fears."
+        , "Be curious. What people call intelligence just boils down to curiousity."
+        , "You can start late, look different, be uncertain and still succeed."
+        , "Nature finds a way, you will to."
+        , "We are what we repeatedly do. Excellence is not an act but a habit."
     ]
     
     var body : some View {
-        Text(headerText)
-            .font(.largeTitle)
-            .foregroundStyle(lightPrimaryColor)
-        
-        Spacer()
-        
-        Text(dailyQuote)
-            .foregroundStyle(lightPrimaryColor)
-            .font(.title)
-            .padding(.vertical, 30)
-            .multilineTextAlignment(.center)
-            .minimumScaleFactor(0.5)
-       
-        Spacer()
-        
-        VStack{
+        VStack(spacing: 0){
             
-            VStack {
-                
-                Text("Week #: \(weekNum + 1)")
-                    .font(.largeTitle)
-                    .padding(.top)
-                    .foregroundStyle(lightFontColor)
-                    .font(.title)
-                
-                Text("Workout #: \(dayOfWeekNum + 1)")
-                    .font(.largeTitle)
-                    .foregroundStyle(lightFontColor)
-                    .font(.title)
-            }
-            .frame(width: 300)
-            .padding(.bottom)
-            .background(lightPrimaryColor)
-            .clipShape(RoundedRectangle(cornerSize: CGSize(width: 10.0, height: 10.0)))
+            Text(headerText)
+                .font(.largeTitle)
+                .foregroundStyle(lightPrimaryColor)
+                .frame(maxWidth:.infinity)
+                .border(.black, width:3)
             
-            VStack {
-                
-                Text("Muscle Groups:")
-                    .font(.largeTitle)
-                    .foregroundStyle(lightFontColor)
-                    .font(.title)
-                
-                // Find the current daily muscle groups
-                
-                let dailyMuscleGroups = muscleGroupsByDay[workoutNum % muscleGroupsByDay.count]
-                
-                Text(dailyMuscleGroups)
-                    .font(.title)
-                    .foregroundStyle(lightFontColor)
-                    .font(.title)
-            }
-            .frame(width: 300)
-            .padding(.bottom)
-            .background(lightPrimaryColor)
-            .clipShape(RoundedRectangle(cornerSize: CGSize(width: 10.0, height: 10.0)))
+            Spacer()
             
-            
-            VStack {
+            Text(dailyQuote)
+                .foregroundStyle(lightPrimaryColor)
+                .font(.title)
+                .padding(.vertical, 30)
+                .multilineTextAlignment(.center)
+                .minimumScaleFactor(0.5)
+                .frame(height: 200)
+                .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+                .border(.black, width:3)
+
+            Spacer()
+           
+            VStack{
                 
-                Text("Number of Sets: \(dailySets)")
-                    .font(.title)
-                    .padding(.top)
-                    .foregroundStyle(lightFontColor)
-                
-            }
-            .frame(width: 300)
-            .padding(.bottom)
-            .background(lightPrimaryColor)
-            .clipShape(RoundedRectangle(cornerSize: CGSize(width: 10.0, height: 10.0)))
-            
-            
-            VStack {
-                
-                Text("Number of Reps: " + dailyReps)
-                    .font(.title)
-                    .padding(.top)
-                    .foregroundStyle(lightFontColor)
-                
-            }
-            .frame(width: 300)
-            .padding(.bottom)
-            .background(lightPrimaryColor)
-            .clipShape(RoundedRectangle(cornerSize: CGSize(width: 10.0, height: 10.0)))
-            
-            VStack {
-                
-                Button("Generate Next Workout") {
-                    // Iterate workout day
-                    workoutNum += 1
-                    dayOfWeekNum += 1
+                VStack {
                     
-                    // Calculate the number of weeks
-                    if dayOfWeekNum > 6 {
-                        weekNum += 1
-                        dayOfWeekNum = 0
-                        
-                        // Find new weekly sets and reps
-                        guard let nextSet = findNextValue(curCount: weekNum, records: numSets) else { return }
-                        dailySets = nextSet
-                        
-                        guard let nextReps = findNextValue(curCount: weekNum, records: numReps) else { return }
-                        dailyReps = nextReps
-                    }
-                    
-                    // Find Daily Muscle Groups
-                    guard let nextQuote = findNextValue(curCount: workoutNum, records: quotes) else { return }
-                    dailyQuote = nextQuote
-                    
+                    Text("Week #: \(weekNum + 1)")
+                        .font(.largeTitle)
+                        .padding(.top)
+                        .foregroundStyle(lightFontColor)
+                        .font(.title)
+                        .border(.black, width:3)
+
+                    Text("Workout #: \(dayOfWeekNum + 1)")
+                        .font(.largeTitle)
+                        .foregroundStyle(lightFontColor)
+                        .font(.title)
+                        .border(.black, width:3)
                 }
+                .frame(width: 300)
+                .padding(.bottom)
+                .background(lightPrimaryColor)
+                .clipShape(RoundedRectangle(cornerSize: CGSize(width: 10.0, height: 10.0)))
+                .border(.black, width:3)
+                
+                VStack {
+                    
+                    Text("Muscle Groups:")
+                        .font(.largeTitle)
+                        .foregroundStyle(lightFontColor)
+                        .font(.title)
+                        .border(.black, width:3)
+
+
+                    // Find the current daily muscle groups
+                    
+                    let dailyMuscleGroups = muscleGroupsByDay[workoutNum % muscleGroupsByDay.count]
+                    
+                    Text(dailyMuscleGroups)
+                        .font(.title)
+                        .foregroundStyle(lightFontColor)
+                        .font(.title)
+                        .border(.black, width:3)
+                }
+                .frame(width: 300)
+                .padding(.bottom)
+                .background(lightPrimaryColor)
+                .clipShape(RoundedRectangle(cornerSize: CGSize(width: 10.0, height: 10.0)))
+                .border(.black, width:3)
+
+                
+                VStack {
+                    
+                    Text("Number of Sets: \(dailySets)")
+                        .font(.title)
+                        .padding(.top)
+                        .foregroundStyle(lightFontColor)
+                        .border(.black, width:3)
+
+                }
+                .frame(width: 300)
+                .padding(.bottom)
+                .background(lightPrimaryColor)
+                .clipShape(RoundedRectangle(cornerSize: CGSize(width: 10.0, height: 10.0)))
+                .border(.black, width:3)
+
+                
+                VStack {
+                    
+                    Text("Number of Reps: " + dailyReps)
+                        .font(.title)
+                        .padding(.top)
+                        .foregroundStyle(lightFontColor)
+                        .border(.black, width:3)
+
+                }
+                .frame(width: 300)
+                .padding(.bottom)
+                .background(lightPrimaryColor)
+                .clipShape(RoundedRectangle(cornerSize: CGSize(width: 10.0, height: 10.0)))
+                .border(.black, width:3)
+
+                VStack {
+                    
+                    Button("Generate Next Workout") {
+                        // Iterate workout day
+                        workoutNum += 1
+                        dayOfWeekNum += 1
+                        
+                        // Calculate the number of weeks
+                        if dayOfWeekNum > 6 {
+                            weekNum += 1
+                            dayOfWeekNum = 0
+                            
+                            // Find new weekly sets and reps
+                            guard let nextSet = findNextValue(curCount: weekNum, records: numSets) else { return }
+                            dailySets = nextSet
+                            
+                            guard let nextReps = findNextValue(curCount: weekNum, records: numReps) else { return }
+                            dailyReps = nextReps
+                        }
+
+                        // Find Daily Muscle Groups
+                        guard let nextQuote = findNextValue(curCount: workoutNum, records: quotes) else { return }
+                        dailyQuote = nextQuote
+                        
+                    }
+                }
+                .buttonStyle(.borderedProminent)
+                .tint(lightSecondaryColor)
+                .font(.title)
+                .border(.black, width:3)
             }
-            .buttonStyle(.borderedProminent)
-            .tint(lightSecondaryColor)
-            .font(.title)
+            .border(.black, width:3)
+            
+            Spacer()
         }
         
-        Spacer()
-        
     }
-    
+
     func findNextValue<T>(curCount: Int, records: [T]) -> T? {
         guard records.count != 0 else {
             return nil
